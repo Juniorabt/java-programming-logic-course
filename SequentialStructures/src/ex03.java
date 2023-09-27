0d3726827fda9358a753d45b39e0f084fab94b11
Juniorabt <ejrabt1@gmail.com>
2023-09-27 19:38:51 -0300

    add casting exercise

---
+++ SequentialStructures/src/ex03.java
@@ -0,0 +1,18 @@
+public class ex03 {
+    public static void main(String[] args) {
+        
+        int a, b;
+        double result, resultCAST;
+
+        a = 5;
+        b = 2;
+
+        result = a / b;
+
+        resultCAST = (double) a / b;
+
+        System.out.println(result);
+        System.out.println(resultCAST);
+
+    }
+}
